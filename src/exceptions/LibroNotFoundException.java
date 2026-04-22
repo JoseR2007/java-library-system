d71a873a517f7b2d1b4b94517571d791cba2c1c6
José Rafael <joserafaeltineog@gmail.com>
2026-04-22 15:27:03 -0400

    Se agregaron 2 exceptiones para el manejo de libros y usuarios existentes

--- src/exceptions/LibroNotFoundException.java
+++ src/exceptions/LibroNotFoundException.java
@@ -1,5 +1,20 @@
 package src.exceptions;
 
 public class LibroNotFoundException extends RuntimeException {
+  private final int idLibroNotFound;
 
+  /**
+   * @param idLibroNotFound (int) Indica el id del libro no encontrado.
+   */
+  public LibroNotFoundException(int idLibroNotFound) {
+    super("El ID indicado no corresponde a ningun libro existente. " + idLibroNotFound);
+    this.idLibroNotFound = idLibroNotFound;
+  }
+
+  /**
+   * @return (int) Retorna el id del libro no encontrado.
+   */
+  public int getIdLibroNotFound() {
+    return this.idLibroNotFound;
+  }
 }
